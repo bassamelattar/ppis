ebeec2cbec20eb5bc3fd56a893a4a2ba356670d7
bassamelattar <130324412+bassamelattar@users.noreply.github.com>
2023-04-10 03:25:15 +0200

    Add files via upload

---
+++ src/main/java/com/mycompany/ppis/PPIS.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ppis;
+
+/**
+ *
+ * @author Bassam El attar
+ */
+import java.util.Scanner;
+public class PPIS {
+public static void main(String[] args) {
+       //System.out.println("Hello World!");
+       /*
+        System.out.println("Enter Level ");
+        Scanner input = new Scanner(System.in);
+        int x =input.nextInt();
+        switch(x){ 
+            case 1:System.out.println("first level");break;
+            case 2:System.out.println("second level");break;
+            case 3:System.out.println("third level");break;
+            case 4:System.out.println("fourth level");break;
+            default:System.out.println("error ");break;
+        }
+        */
+       /*
+       System.out.println("Enter grade ");
+        Scanner input = new Scanner(System.in);
+        String x=input.next();
+          switch(x){ 
+            case "a","A":System.out.println("Exellent");break;
+            case "b":
+            case "B":System.out.println("Very good");break;
+            case "c","C":System.out.println("good");break;
+            default:System.out.println("error ");break;
+          }
+        */   
+      
+       /*     
+       System.out.println("Enter number 1 ");
+        
+         int x =input.nextInt(); 
+            System.out.println("Enter Number 2 ");
+        int z =input.nextInt(); 
+       
+        if(z==x){
+            System.out.println("Square");
+           
+        }
+        else{
+            System.out.println("not Square");
+        }
+       */
+     
+       
+    
+       
+        
+        
+    }
+}
